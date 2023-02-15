65184064fb8c04d94e9e291fe21f95668b1bcaff
steph4041 <82727066+steph4041@users.noreply.github.com>
2023-02-15 17:10:34 +0100

    Update THE_COCK_STRETCHER.cpp
    cum

--- THE_COCK_STRETCHER.cpp
+++ THE_COCK_STRETCHER.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
 #include <stdio.h>
 #include <string.h>
-
+#include <windows.h>
 using namespace std;
-
 int main()
 {
-    using namespace std::this_thread; // sleep_for, sleep_until
-    using namespace std::chrono; // nanoseconds, system_clock, seconds
-
     char lel[1000000] = "";
     char extension[] = "=";
-
     for(int i=0; i > -1; i++)
     {
         strcat(lel, extension);
         printf(" my cock now is long: %d cm \n", i);
         printf("8%sD \n \n", lel);
-        sleep_for(nanoseconds(1000000000));
+        Sleep(0.0001);
     }
 }
