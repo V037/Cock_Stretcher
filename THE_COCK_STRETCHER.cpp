1c522f414590d7f7408a81e79ee539860441aac7
Valentino <69222741+V037@users.noreply.github.com>
2023-02-15 17:57:24 +0100

    credits

--- THE_COCK_STRETCHER.cpp
+++ THE_COCK_STRETCHER.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <windows.h>
 using namespace std;
+//vcringe37_, stepsister404
 int main()
 {
     char lel[1000000] = "";
@@ -10,7 +11,7 @@ int main()
     for(int i=0; i > -1; i++)
     {
         strcat(lel, extension);
-        printf(" my cock now is long: %d cm \n", i);
+        printf(" my cock now is long: %d cm \n", i-1);
         printf("8%sD \n \n", lel);
         Sleep(0.0001);
     }
