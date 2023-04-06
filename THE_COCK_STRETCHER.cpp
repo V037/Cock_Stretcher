2df90a7b4325f47555835a34883084718ef6b91a
Valentino <69222741+V037@users.noreply.github.com>
2023-04-06 21:29:35 +0200

    added description

--- THE_COCK_STRETCHER.cpp
+++ THE_COCK_STRETCHER.cpp
@@ -6,13 +6,13 @@ using namespace std;
 //credits: steph404, V037_
 int main()
 {
-    char lel[1000000] = "";
-    char extension[] = "=";
-    for(int i=0; i > -1; i++)
+    char lel[1000000] = ""; //allocating memory
+    char extension[] = "="; //making the cock bigger
+    for(int i=0; i > -1; i++)   //doing a loop
     {
-        strcat(lel, extension);
-        printf(" my cock now is long: %d cm \n", i-1);
+        strcat(lel, extension);                         //doing the big math
+        printf(" my cock now is long: %d cm \n", i-1);  //print results
         printf("8%sD \n \n", lel);
-        Sleep(0.0001);
+        Sleep(0.0001);                                  //cooldown
     }
 }
