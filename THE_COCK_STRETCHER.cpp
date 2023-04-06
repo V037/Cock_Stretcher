356d888f5140ebc70074c8e105886f25910aeace
Valentino <69222741+V037@users.noreply.github.com>
2023-04-06 21:30:52 +0200

    description

--- THE_COCK_STRETCHER.cpp
+++ THE_COCK_STRETCHER.cpp
@@ -6,8 +6,8 @@ using namespace std;
 //credits: steph404, V037_
 int main()
 {
-    char lel[1000000] = ""; //allocating memory
-    char extension[] = "="; //making the cock bigger
+    char lel[1000000] = ""; //allocating memory, char stands for 1 character of alphabet in Ascii
+    char extension[] = "="; //how the extension looks like
     for(int i=0; i > -1; i++)   //doing a loop
     {
         strcat(lel, extension);                         //doing the big math
